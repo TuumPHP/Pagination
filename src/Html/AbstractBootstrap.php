785300e4b61f42b5a1be3683369ac99715b55d9b
Asao <kamei@workspot.jp>
2015-06-26 16:09:30 +0900

    moved many methods to abstract class.

--- src/Html/AbstractBootstrap.php
+++ src/Html/AbstractBootstrap.php
@@ -26,6 +26,11 @@ abstract class AbstractBootstrap
     protected $options = [];
 
     /**
+     * @var array
+     */
+    protected $sr_label = [];
+
+    /**
      * @var string
      */
     public $ul_class = 'pagination';
@@ -45,18 +50,6 @@ abstract class AbstractBootstrap
         return $self;
     }
 
-    /**
-     * @param array $info
-     * @return string
-     */
-    abstract protected function listItem(array $info);
-
-    /**
-     * @param int $numLinks
-     * @return array
-     */
-    abstract protected function calculatePagination($numLinks);
-    
     /**
      * @return string
      */
@@ -108,6 +101,65 @@ abstract class AbstractBootstrap
             return (integer)(ceil($total / $pages));
         }
         return $this->currPage + $numLinks;
+    }
+
+    /**
+     * @param int $numLinks
+     * @return array
+     */
+    protected function calculatePagination($numLinks = 5)
+    {
+        $lastPage = $this->findLastPage($numLinks);
+        $currPage = $this->inputs->getCurrPage();
+
+        $pages   = [];
+        $pages[] = ['label' => 'top',  'page' => 1]; // top
+        $pages[] = ['label' => 'prev', 'page' => max($currPage - 1, 1)]; // prev
+
+        // list of pages, from $start till $last.
+        $pages   = array_merge($pages, $this->fillPages($numLinks));
+
+        $pages[] = ['label' => 'next', 'page' => min($currPage + 1, $lastPage)]; // next
+        $pages[] = ['label' => 'last', 'page' => $lastPage]; // last
+        return $pages;
+    }
+
+    /**
+     * @param string $label
+     * @param int    $page
+     * @param string $type
+     * @return string
+     */
+    protected function bootLi($label, $page, $type = 'disable')
+    {
+        $label = isset($this->options[$label]) ? $this->options[$label] : $label;
+        if (isset($this->sr_label[$label])) {
+            $srLbl = " aria-label=\"{$this->sr_label[$label]}\"";
+        } else {
+            $srLbl = '';
+        }
+        if ($page != $this->currPage) {
+            $key  = $this->inputs->pagerKey;
+            $html = "<li><a href='{$this->getRequestUri()}{$key}={$page}'";
+            $html .= $srLbl. " >{$label}</a></li>\n";
+        } elseif ($type == 'disable') {
+            $html = "<li class='disabled'><a href='#' >{$label}</a></li>\n";
+        } else {
+            $html = "<li class='active'><a href='#' >{$label}</a></li>\n";
+        }
+        return $html;
+    }
+
+    /**
+     * @param array $info
+     * @return string
+     */
+    protected function listItem(array $info)
+    {
+        $label = isset($info['label']) ? $info['label'] : '';
+        $page  = isset($info['page']) ? $info['page'] : '';
+        $type  = isset($info['type']) ? $info['type'] : '';
+        return $this->bootLi($label, $page, $type);
     }
 
 }
